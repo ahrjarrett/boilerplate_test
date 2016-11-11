bc622652b63cdab921c60700723b02ebc886934f
Andrew Jarrett <aj@Andrews-MacBook-Air.local>
2016-11-11 15:08:45 -0700

    initial commit

---
+++ es6.js
@@ -0,0 +1,5 @@
+require('babel/register')({
+  extensions: ['.js'],
+  ignore: /node_modules/
+});
+
---
+++ test/first_test.js
@@ -0,0 +1,9 @@
+var R = require('ramda');
+var _ = require('lodash');
+var testData = require('./data.json');
+
+describe("first test", () => {
+  it("will fail", () => {
+    false.should.eql(true);
+  });
+});
