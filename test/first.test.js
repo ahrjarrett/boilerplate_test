411f547a61c9117abba5b3928171806238863da2
Andrew Jarrett <ahrjarrett@gmail.com>
2016-11-28 21:18:52 -0700

    wrote ramda filter test, moved data to separate dir

--- test/first.test.js
+++ test/first.test.js
@@ -1,6 +1,6 @@
-var R = require('ramda')
-var _ = require('lodash')
-var testData = require('./data.json')
+const R = require('ramda')
+const _ = require('lodash')
+const testData = require('../data/data.json')
 
 describe("fail test", () => {
   it("will fail", () => {
@@ -14,5 +14,21 @@ describe("_.isString", () => {
   })
 })
 
+describe("ramda identity", () => {
+  it("identity should return true", () => {
+    R.identity(testData).should.eql(testData)
+  })
+})
 
+describe("ramda filter", () => {
+  it("should filter array to odd numbers", () => {
+    const isOdd = (n) => n % 2 === 1
+    R.filter(isOdd, [1,2,3,4,5,6,7]).should.eql([1,3,5,7])
+  })
+})
+
+//describe("ramda mapping", () => {
+//  it("should map toUpperCase to name", () => {
+//  })
+//})
 
