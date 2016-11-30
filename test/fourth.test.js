be16fc8b72285c63f1071d3d3cd100dfb0d19354
Andrew Jarrett <ahrjarrett@gmail.com>
2016-11-30 13:08:00 -0700

    fourth test passed, need to refactor some

---
+++ test/fourth.test.js
@@ -0,0 +1,33 @@
+// not using Ramda this time, only vanilla map, filter, etc.
+import { problem, solution } from '../data/mockData_4.js'
+
+Array.prototype.concatAll = function() {
+  var results = []
+  this.forEach(function(subArray) {
+    subArray.forEach(function (item){
+      results.push(item)
+    })
+  })
+  return results
+}
+
+function movieTest() {
+  const movieLists = problem
+
+  return movieLists.map(function(movieList) {
+    return movieList.videos.map(function(video) {
+      return video.boxarts.filter(function(boxart){
+        return boxart.width === 150
+      })
+      .map(function(boxart){
+        return { id: video.id, title: video.title, boxart: boxart.url }
+      })
+    }).concatAll()
+  }).concatAll()
+}
+
+describe("vanilla JS: ", () => {
+  it.only('fails', () => {
+    movieTest().sort().should.eql(solution.sort())
+  })
+})
