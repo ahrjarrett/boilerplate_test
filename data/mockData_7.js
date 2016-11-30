a84a05e919d8462aec7f75cd873a1cd39631bf16
Andrew Jarrett <ahrjarrett@gmail.com>
2016-11-30 16:09:32 -0700

    introduced bug in test#5 by redefining Array.prototype.reduce in #8

---
+++ data/mockData_7.js
@@ -0,0 +1,6 @@
+// object at index[3] has the largest dimensions, which is what we'll test against
+export const problem = [
+  { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+  { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+  { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+  { width: 425, height: 150, url: "http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" } ]
