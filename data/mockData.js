2b2f5e34877ab3d0467f4f8d929819d589318e45
Andrew Jarrett <ahrjarrett@gmail.com>
2016-11-30 06:08:36 -0700

    second test passes; need to break into smaller tests

--- data/mockData.js
+++ data/mockData.js
@@ -9,6 +9,12 @@ export const data = {
 
 export const goal = {
   group1: [
-    { value: 'group1-perm1', checked: true, 'label': 'perm1'}
-  ]
+    { value: 'group1-perm1', checked: true, 'label': 'perm1'},
+    { value: 'group1-perm2', checked: false, 'label': 'perm2'} ],
+  group2: [
+    { value: 'group2-perm1', checked: false, 'label': 'perm1'},
+    { value: 'group2-perm2', checked: true, 'label': 'perm2'} ],
+  general: [
+    { value: 'perm3', checked: true, 'label': 'perm3'},
+    { value: 'perm4', checked: false, 'label': 'perm4'} ]
 }
--- test/second.test.js
+++ test/second.test.js
@@ -12,18 +12,24 @@ describe("data should match goal", () => {
   it.only("sensual data massage is sensual", () => {
 
     const GROUP_LEN = 6
-    const inGroup = str => str.indexOf('-') === GROUP_LEN
-
+    const KEYS = ['value', 'checked', 'label']
+    const inGroup = (str) => str.indexOf('-') === GROUP_LEN
     const getLabel = R.pipe(R.splitAt(GROUP_LEN + 1), R.last)
-
     const addLabel = R.chain(R.compose(
       R.ifElse(inGroup, getLabel, R.identity),
       R.head), R.append
     )
+    const convert = R.pipe(addLabel, R.zipObj(KEYS))
+    const groupName = R.ifElse(R.pipe(R.prop('value'), inGroup),
+      R.pipe(R.prop('value'), R.splitAt(GROUP_LEN), R.head),
+      () => 'general'
+    )
 
     const massageData = R.pipe(
       R.toPairs,
-      R.map(addLabel)
+      R.map(convert),
+      R.groupBy(groupName),
+      R.tap(console.log)
     )
 
     massageData(data).should.eql(goal)
