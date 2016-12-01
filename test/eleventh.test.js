fa415d7b470b734295f9095b9d3beeb17f73eaa0
Andrew Jarrett <ahrjarrett@gmail.com>
2016-11-30 18:20:45 -0700

    refactoring zip.test.js (still named 11th.test.js)

--- test/eleventh.test.js
+++ test/eleventh.test.js
@@ -1,7 +1,7 @@
 const left = [1,4,5], right = [1,7,5]
 const addLeftToRight = (left, right) => left + right
 
-// ramda does the loop a little differently:
+// ramda does the looping a little differently:
 // they use a while loop, & instead of pushing to result
 // they do: `result[idx] = [l[idx], r[idx]]`
 Array.zip = function(l, r, fn){
@@ -14,7 +14,7 @@ Array.zip = function(l, r, fn){
 }
 
 describe('11: wrote a naive zip fn that:', () => {
-  it('should zip her up', () => {
+  it.only('should zip her up', () => {
     Array.zip(left, right, addLeftToRight).should.eql([2, 11, 10])
   })
 })
