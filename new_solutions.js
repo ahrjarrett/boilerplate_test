ea4ab2e02c7fecf0554596a2c9153c4489e39469
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-10 18:59:21 -0700

    finish problems 11-26 using ES6; observables left

--- new_solutions.js
+++ new_solutions.js
@@ -589,6 +589,132 @@ function() {
 		
 
 // 25: Converting from Arrays to Trees
+function() {
+
+  var lists = [
+    {
+      "id": 5434364,
+      "name": "New Releases"
+    },
+    {
+      "id": 65456475,
+      "name": "Thrillers"
+    }
+  ]
+  var videos = [
+    {
+      "listId": 5434364,
+      "id": 65432445,
+      "title": "The Chamber"
+    },
+    {
+      "listId": 5434364,
+      "id": 675465,
+      "title": "Fracture"
+    },
+    {
+      "listId": 65456475,
+      "id": 70111470,
+      "title": "Die Hard"
+    },
+    {
+      "listId": 65456475,
+      "id": 654356453,
+      "title": "Bad Boys"
+    }
+  ];
+
+  // Solution:
+  return lists.map(list => ({
+    name: list.name,
+    videos: videos
+      .filter(({ listId }) => listId === list.id)
+      .map(({ id, title }) => ({ id, title }))
+  }))
+
+}
+		
+
+// 26: Converting from Arrays to Deeper Trees
+function() {
+
+  // Input:
+  var lists = [
+    {
+      "id": 5434364,
+      "name": "New Releases"
+    },
+    {
+      "id": 65456475,
+      name: "Thrillers"
+    }
+  ]
+  var videos = [
+    {
+      "listId": 5434364,
+      "id": 65432445,
+      "title": "The Chamber"
+    },
+    {
+      "listId": 5434364,
+      "id": 675465,
+      "title": "Fracture"
+    },
+    {
+      "listId": 65456475,
+      "id": 70111470,
+      "title": "Die Hard"
+    },
+    {
+      "listId": 65456475,
+      "id": 654356453,
+      "title": "Bad Boys"
+    }
+  ]
+  var boxarts = [
+    { videoId: 65432445, width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+    { videoId: 65432445, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
+    { videoId: 675465, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+    { videoId: 675465, width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+    { videoId: 675465, width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+    { videoId: 70111470, width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+    { videoId: 70111470, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
+    { videoId: 654356453, width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+    { videoId: 654356453, width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+  ]
+  var bookmarks = [
+    { videoId: 65432445, time: 32432 },
+    { videoId: 675465, time: 3534543 },
+    { videoId: 70111470, time: 645243 },
+    { videoId: 654356453, time: 984934 }
+  ];
+  
+  // Solution:
+  return lists.map(list => ({
+    name: list.name,
+    videos: videos
+      .filter(video => video.listId === list.id)
+      .concatMap(({ id, title }) => (
+	Array.zip(
+	  bookmarks.filter(bookmark => bookmark.videoId === id),
+	  boxarts
+	    .filter(boxart => boxart.videoId === id)
+	    .reduce((acc, curr) => (
+	      acc.width * acc.height < curr.width * curr.height ? acc : curr
+	    )
+          ),
+	  ({ time }, { url }) => ({
+	    id,
+	    title,
+	    time,
+	    boxart: url
+	  })  
+        )
+      ))
+    })
+  )
+
+}
 
 
 
